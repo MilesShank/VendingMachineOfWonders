0c73d7f1667f2df1e8015d313701702259d8c8e3
alisyn-schilling <aeschilling.87@gmail.com>
2020-07-22 17:08:54 -0400

    Vending Machine can reject invalid coins

--- src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
+++ src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
@@ -1,6 +1,8 @@
 package com.fizzbuzzcola.vendingmachine;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class VendingMachine {
 
@@ -8,16 +10,22 @@ public class VendingMachine {
     private BigDecimal quarter;
     private BigDecimal dime;
     private BigDecimal nickel;
+    private Collection<String> coinReturn;
 
     public VendingMachine(BigDecimal totalMoney) {
         this.totalMoney = totalMoney;
         this.quarter = new BigDecimal(".25");
         this.dime = new BigDecimal(".10");
         this.nickel = new BigDecimal(".05");
+        this.coinReturn = new ArrayList<>();
     }
 
     public BigDecimal getTotalMoney() {
         return totalMoney;
+    }
+
+    public Collection<String> getCoinReturn() {
+        return coinReturn;
     }
 
     public void acceptCoin(String insertedCoin) {
@@ -27,6 +35,8 @@ public class VendingMachine {
             totalMoney = totalMoney.add(dime);
         } else if (insertedCoin.equalsIgnoreCase("nickel")){
             totalMoney = totalMoney.add(nickel);
+        } else {
+            coinReturn.add(insertedCoin);
         }
     }
 }
--- src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineApplicationTests.java
+++ src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineApplicationTests.java
@@ -1,14 +1,23 @@
 package com.fizzbuzzcola.vendingmachine;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.Collection;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class VendingMachineApplicationTests {
+    private VendingMachine underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new VendingMachine(new BigDecimal("10.10"));
+    }
 
     @Test
     void contextLoads() {
@@ -22,8 +31,13 @@ class VendingMachineApplicationTests {
 
     @Test
     public void vendingMachineShouldAcceptCoins() {
-        VendingMachine underTest = new VendingMachine(new BigDecimal("10.10"));
         underTest.acceptCoin("quarter");
         assertEquals(BigDecimal.valueOf(10.35), underTest.getTotalMoney());
+    }
+
+    @Test
+    public void vendingMachineShouldRejectInvalidCoins() {
+        underTest.acceptCoin("apple");
+        assertThat(underTest.getCoinReturn()).containsExactlyInAnyOrder("apple");
     }
 }
