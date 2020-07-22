4efd396e9290987cf7421f59864f2e286df54e47
alisyn-schilling <aeschilling.87@gmail.com>
2020-07-22 16:50:47 -0400

    Vending Machine Can Accept Coins

--- src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
+++ src/main/java/com/fizzbuzzcola/vendingmachine/VendingMachine.java
@@ -3,13 +3,30 @@ package com.fizzbuzzcola.vendingmachine;
 import java.math.BigDecimal;
 
 public class VendingMachine {
+
     private BigDecimal totalMoney;
+    private BigDecimal quarter;
+    private BigDecimal dime;
+    private BigDecimal nickel;
 
     public VendingMachine(BigDecimal totalMoney) {
         this.totalMoney = totalMoney;
+        this.quarter = new BigDecimal(".25");
+        this.dime = new BigDecimal(".10");
+        this.nickel = new BigDecimal(".05");
     }
 
     public BigDecimal getTotalMoney() {
         return totalMoney;
+    }
+
+    public void acceptCoin(String insertedCoin) {
+        if (insertedCoin.equalsIgnoreCase("quarter")) {
+            totalMoney = totalMoney.add(quarter);
+        } else if (insertedCoin.equalsIgnoreCase("dime")) {
+            totalMoney = totalMoney.add(dime);
+        } else if (insertedCoin.equalsIgnoreCase("nickel")){
+            totalMoney = totalMoney.add(nickel);
+        }
     }
 }
--- src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineApplicationTests.java
+++ src/test/java/com/fizzbuzzcola/vendingmachine/VendingMachineApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class VendingMachineApplicationTests {
 
@@ -16,5 +18,12 @@ class VendingMachineApplicationTests {
     @Test
     public void vendingMachineShouldExist() {
         VendingMachine underTest = new VendingMachine(new BigDecimal("10.10"));
+    }
+
+    @Test
+    public void vendingMachineShouldAcceptCoins() {
+        VendingMachine underTest = new VendingMachine(new BigDecimal("10.10"));
+        underTest.acceptCoin("quarter");
+        assertEquals(BigDecimal.valueOf(10.35), underTest.getTotalMoney());
     }
 }
